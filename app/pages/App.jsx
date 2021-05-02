184a608c4f9db1bf50c2c6049d669c3af113faad
pavittarx <29155477+pavittarx@users.noreply.github.com>
2021-05-02 21:10:37 +0530

    orders UI init

--- app/pages/App.jsx
+++ app/pages/App.jsx
@@ -1,42 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Button } from "semantic-ui-react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 
 import "semantic-ui-css/semantic.min.css";
 import "./app.scss";
 
 import { LoginUI, RegisterUI } from "./auth/auth";
+import {authenticate} from "./../libs/auth";
 import Context from "./context";
 
 const Header = () => {
+  const { formStatus, setFormStatus } = useContext(Context);
+
+  const changeFormStatus = () => {
+    formStatus? setFormStatus(0) : setFormStatus(1);
+  }
+
   return (
     <section className="header">
       <div className="left-block logo">OrderBook</div>
       <div className="right-block">
-        <Button primary content="Create Account" />
+        <Button
+          primary
+          content={formStatus ? "Create Account" : "Login"}
+          onClick={changeFormStatus}
+        />
       </div>
     </section>
   );
 };
 
-const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // 1 - Show Login Form, 0 - Show Create Account Form
-  const [formStatus, setFormStatus] = useState(0);
 
-  const value = {isLoggedIn, formStatus, setIsLoggedIn, setFormStatus};
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(authenticate());
+  // 1 - Show Login Form, 0 - Show Create Account Form
+  const [formStatus, setFormStatus] = useState(1);
+
+  console.log(isLoggedIn);
+
+  const value = { isLoggedIn, formStatus, setIsLoggedIn, setFormStatus };
+
+  const MainForm = () => {
+    if(formStatus) return <LoginUI />;
+    return <RegisterUI />;
+  }
 
   return (
     <Context.Provider value={value}>
       <Header />
-      <Router> 
+      <Router>
         <Switch>
+          <Route path="/orders">
+            {isLoggedIn? <div> Hello Orders </div> : <Redirect to="/" />}
+          </Route>
           <Route path="/">
-            { formStatus? <LoginUI /> : <RegisterUI /> }
+            {isLoggedIn? <Redirect to="/orders" /> : MainForm() }
           </Route>
         </Switch>
       </Router>
-      </Context.Provider>
+    </Context.Provider>
   );
 };
 
