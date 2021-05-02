41cc4793c605a49885d2e6e49525100b3e959fa9
pavittarx <29155477+pavittarx@users.noreply.github.com>
2021-05-02 21:11:07 +0530

    formatting fix

--- server/libs/auth.js
+++ server/libs/auth.js
@@ -1,27 +1,32 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
-const {Users} = require("./../db/collections");
-
+const { Users } = require("./../db/collections");
 
 // Genrates Authorization JWT token for the  user
 const authorize = async (usernameOrEmail, password) => {
-  if(!usernameOrEmail && !password) 
+  if (!usernameOrEmail && !password)
     throw new Error("Missing Credentials: Please provide username, password.");
 
   const usersCollection = await Users();
 
   const query = {
-    $or: [{
-      username: usernameOrEmail
-    }, {email: usernameOrEmail}]
-  }
+    $or: [
+      {
+        username: usernameOrEmail,
+      },
+      { email: usernameOrEmail },
+    ],
+  };
 
   const user = await usersCollection.findOne(query);
 
   console.log(password, user);
 
-  if(!user) throw new Error(`The user with username: "${usernameOrEmail}" does not exist.`);
+  if (!user)
+    throw new Error(
+      `The user with username: "${usernameOrEmail}" does not exist.`
+    );
 
   if (!(await bcrypt.compare(password, user.passHash))) {
     throw new Error(`Incorrect username or password.`);
@@ -30,27 +35,29 @@ const authorize = async (usernameOrEmail, password) => {
   const token = await jwt.sign(
     {
       id: user._id,
-      username: user.username
+      username: user.username,
     },
     user.passHash
   );
 
   return token;
-}
+};
 
 // Checks if the token provided is authentic
 const authenticate = async (token) => {
   const tokenData = await jwt.decode(token);
 
-  if(!tokenData){
+  if (!tokenData) {
     throw new Error("Invalid Token");
   }
 
   const users = await Users();
-  const user = await users.findOne({username: tokenData.username});
+  const user = await users.findOne({ username: tokenData.username });
 
-  if(!user){
-    throw new Error(`Your token seems to have been expired. Please log in to continue.`);
+  if (!user) {
+    throw new Error(
+      `Your token seems to have been expired. Please log in to continue.`
+    );
   }
 
   return {
@@ -58,11 +65,11 @@ const authenticate = async (token) => {
     data: {
       id: user._id,
       username: user.username,
-    }
+    },
   };
-}
+};
 
 module.exports = {
   authorize,
-  authenticate
-}
+  authenticate,
+};
