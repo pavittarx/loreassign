e212833723bd837d63fa3eec8f3e8b9632f159eb
pavittarx <29155477+pavittarx@users.noreply.github.com>
2021-05-02 22:45:05 +0530

    deploy fix

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Listening ...");
 });
